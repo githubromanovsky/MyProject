f40dfc2fdafbe72eec6b92a69646593acec8d3f6
Alexandr Romanovsky <githubromanovskiy@gmail.com>
2020-07-11 20:03:35 +0300

    Change description

--- scanport.py
+++ scanport.py
@@ -27,12 +27,10 @@ def scan_not_list_port():
 def scan_list_port():
     host = input("host:")
     print(colored("Enter a list elements separated by space", 'yellow'))
-    # port = []
     # Create list ports
     port = [int(item) for item in input('Port:').split()]
-    # port = [89000, 20, 22, 23, 21, 666, 45, 6942]
-    # Проходим в цикле по списку портов
 
+    # Loop through the list of ports
     for i in port:
         scan = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # Catch exceptions
@@ -65,10 +63,10 @@ while True:
     print(colored(("*" * 50), 'blue'))
     text_a = input("[Scan]:")
     if text_a == "1":
-        scan_list_port()
+        scan_not_list_port()
         continue
     elif text_a == "2":
-        scan_not_list_port()
+        scan_list_port()
         continue
     elif text_a == "3":
         exit()
